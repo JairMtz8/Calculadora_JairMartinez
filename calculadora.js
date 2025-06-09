7272ada876bb595145f3b0262fd075bc57f43623
Jair Antonio Martínez Valladares <20223tn068@utez.edu.mx>
2025-06-09 17:50:43 -0600

    Calculadora funcional

---
+++ calculadora.js
@@ -0,0 +1,21 @@
+function suma(a, b) {
+    const resultado = a + b;
+    console.log(`El resultado es ${resultado}`)
+}
+
+function resta(a, b) {
+    const resultado = a - b;
+    console.log(`El resultado es ${resultado}`)
+}
+
+function multiplicacion(a, b) {
+    const resultado = a * b;
+    console.log(`El resultado es ${resultado}`)
+}
+
+function division(a, b) {
+    const resultado = a / b;
+    console.log(`El resultado es ${resultado}`)
+}
+
+const result = multiplicacion(1,2)
